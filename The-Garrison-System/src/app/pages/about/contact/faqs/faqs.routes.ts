714572cddf353c0d0594f9eee3f4f6ce891d23e4
LucaDelpra <delpratoluca2018@gmail.com>
2025-10-01 17:47:42 -0300

    agregar páginas estáticas (Sobre nosotros, Contáctanos, FAQs)
    Se implementaron nuevas rutas en app.routes.ts y navegación en app.component.html
    para las páginas estáticas: Sobre nosotros, Contáctanos y FAQs.
    Incluye estructura inicial en /src/app/pages.

---
+++ The-Garrison-System/src/app/pages/about/contact/faqs/faqs.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { FaqsComponent } from './faqs.component';
+
+
+export const FAQS_ROUTES: Routes = [
+{ path: '', component: FaqsComponent, title: 'Preguntas frecuentes' }
+];
